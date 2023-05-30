733485fa0d59494199e03796a1d987bef5f431f3
loofehtt <phu832001@gmail.com>
2023-05-30 12:52:09 +0700

    login, resetpass guestlayout

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+
+
 
 
 const routes = [
+
     {
         path: '/dashboard',
         name: 'dashboard',
@@ -14,7 +19,19 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
-    }
+    },
+
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword
+    },
+
+    {
+        path: '/reset-password/:token',
+        name: 'resetPassword',
+        component: ResetPassword
+    },
 ];
 
 const router = createRouter({
--- backend/src/views/RequestPassword.vue
+++ backend/src/views/RequestPassword.vue
@@ -1,5 +1,5 @@
 <template>
-    <GuestLayout title="Sign in to your account">
+    <GuestLayout title="Request your password">
         <form class="space-y-6" action="#" method="POST">
             <div>
                 <label
@@ -19,50 +19,23 @@
             </div>
 
             <div>
-                <div class="flex items-center justify-between">
-                    <label
-                        for="password"
-                        class="block text-sm font-medium leading-6 text-gray-900"
-                        >Password</label
-                    >
-                    <div class="text-sm">
-                        <router-link
-                            :to="{ name: 'requestPassword' }"
-                            class="font-medium text-indigo-600 hover:text-indigo-500"
-                        >
-                            Forgot your password?
-                        </router-link>
-                    </div>
-                </div>
-                <div class="mt-2">
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autocomplete="current-password"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-            </div>
-
-            <div>
                 <button
                     type="submit"
                     class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    Sign in
+                    Submit
                 </button>
             </div>
         </form>
+
         <p class="mt-10 text-center text-sm text-gray-500">
-            Not a member?
+            Remembered it?
             {{ " " }}
-            <router-link
-                :to="{ name: 'requestPassword' }"
+            <a
+                href="login"
                 class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                >Sign in</a
             >
-                Sign up
-            </router-link>
         </p>
     </GuestLayout>
 </template>
